7c00166700c11b2215fbdc32c0ee49f427e955ad
Aanshi Dobariya <aanshidobariya.107@gmail.com>
2025-12-22 11:37:09 +0530

    push

--- react-webpages/src/components/header/Header.jsx
+++ react-webpages/src/components/header/Header.jsx
@@ -44,10 +44,10 @@ function Header({ setSidebarOpen }) {
             border border-white/20 rounded-2xl px-4 lg:px-6 py-2 lg:py-3
             w-40 h-auto lg:w-80 hover:bg-white/20 transition-all"
           >
-            <Search className="text-white/50 mr-3" />
+            <Search className="text-white/50 mr-3 " />
             <input
               type="text"
-              className="bg-transparent text-white placeholder-white/50
+              className="bg-transparent  text-white placeholder-white/50
               focus:outline-none flex-1 text-sm"
               placeholder="Search..."
             />
